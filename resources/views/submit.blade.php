36fc264fc6c071ecc4bd13bd4ce890f51466b48f
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-08-06 14:42:04 +0200

    hhhtml

--- resources/views/submit.blade.php
+++ resources/views/submit.blade.php
@@ -4,20 +4,22 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Submit | Larasites.com</title>
+        <title>Submit a site | Larasites.com</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     </head>
     <body>
-        <br>
         <div class="container">
-            <p class="lead">Submit a site</p>
+            <br>
+            <p><h5>{!! Html::link('/', 'Larasites.com') !!}</h5></p>
+            <hr>
             {!! Form::open() !!}
             <div class="form-group">
-                <label for="url">URL</label>
-                {!! Form::input('url', 'url', null, ['id' => 'url', 'class' => 'form-control', 'required' => 'required', 'autofocus' => 'autofocus', 'placeholder' => 'eg. http://laravel.com']) !!}
+                {!! Form::input('url', 'url', null, ['class' => 'form-control', 'required' => 'required', 'autofocus' => 'autofocus', 'placeholder' => 'eg. http://laravel.com']) !!}
             </div>
             {!! Form::submit(null, ['class' => 'btn btn-default']) !!}
             {!! Form::close() !!}
+            <hr>
+            <p class="text-muted">Made by {!! Html::link('http://www.wearenext.co.za', 'Next') !!} in Cape Town</p>
         </div>
     </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,20 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
-        <title>Laravel</title>
-
-        <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Larasites.com</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     </head>
     <body>
         <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-            </div>
+            <br>
+            <p><h5>{!! Html::link('/', 'Larasites.com') !!}</h5></p>
+            <hr>
+            <p>{!! Html::link('/submit', 'Submit a site', ['class' => 'btn btn-default']) !!}</p>
+            <hr>
+            <p class="text-muted">Made by {!! Html::link('http://www.wearenext.co.za', 'Next') !!} in Cape Town</p>
         </div>
     </body>
 </html>
