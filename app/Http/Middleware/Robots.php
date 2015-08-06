8a5f80f20f84932a6b1af2c9d05be4733c2c7104
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-08-06 14:22:52 +0200

    robots

---
+++ app/Http/Middleware/Robots.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Robots
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        switch (app()->environment()) {
+            case 'production':
+                if ($request->is('robots.txt')) {
+                    return response('User-Agent: *', 200)
+                           ->header('Content-Type', 'text/plain');
+                }
+            default:
+                if ($request->is('robots.txt')) {
+                    return response("User-Agent: *\nDisallow: /", 200)
+                           ->header('Content-Type', 'text/plain');
+                }
+
+                $response = $next($request);
+
+                $response->header('X-Robots-Tag', 'noindex, nofollow, noarchive');
+
+                return $response;
+        }
+
+        return $next($request);
+    }
+}
