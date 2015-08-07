aa47142e4aa5a690890757bf2917c588b7487461
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-08-07 18:00:17 +0200

    markup

--- resources/views/submissions.blade.php
+++ resources/views/submissions.blade.php
@@ -4,17 +4,19 @@
     @forelse ($hosts as $host)
         <p>
             <h4>{!! Html::link('http://' . $host->name, $host->name) !!}</h4>
-            @foreach ($host->submissions->sortByDesc('created_at')->take(2) as $submission)
-                <blockquote>
-                    {!! Html::link('https://www.twitter.com/@' . $submission->user->twitter_nickname, '@'.$submission->user->twitter_nickname) !!}
-                    submitted
-                    {!! Html::link($submission->url) !!}
-                    <time class="timeago" datetime="{{ $submission->created_at->toIso8601String() }}"></time>
-                </blockquote>
-            @endforeach
-            @if ($host->submissions->count() > 2)
-                <blockquote>+{{ $host->submissions->count() - 2 }} more…</blockquote>
-            @endif
+            <ul>
+                @foreach ($host->submissions->sortByDesc('created_at')->take(2) as $submission)
+                    <li>
+                        {!! Html::link('https://www.twitter.com/@' . $submission->user->twitter_nickname, '@'.$submission->user->twitter_nickname) !!}
+                        submitted
+                        {!! Html::link($submission->url) !!}
+                        <time class="timeago" datetime="{{ $submission->created_at->toIso8601String() }}"></time>
+                    </li>
+                @endforeach
+                @if ($host->submissions->count() > 2)
+                    <li>+{{ $host->submissions->count() - 2 }} more…</li>
+                @endif
+            </ul>
             <a href="#" class="btn btn-default btn-sm">Add Site</a>
             <a href="#" class="btn btn-default btn-sm">Remove</a>
         </p>
