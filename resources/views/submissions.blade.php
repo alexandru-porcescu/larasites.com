cf6cb6910808d700904a85f5c0b30b617da4008d
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-08-07 13:26:42 +0200

    markup

--- resources/views/submissions.blade.php
+++ resources/views/submissions.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
 @foreach ($submissions as $i => $submission)
-    {!! Html::link('https://www.twitter.com/@' . $submission->user->twitter_nickname, '@' . $submission->user->twitter_nickname) !!}
-    submitted
-    {!! Html::link($submission->extraction->url) !!}
-    <time class="timeago" datetime="{{ $submission->created_at->toIso8601String() }}"></time>
-    @if (isset($submissions[$i + 1]))
-        <hr>
-    @endif
+    <p>
+        {!! Html::link('https://www.twitter.com/@' . $submission->user->twitter_nickname, '@' . $submission->user->twitter_nickname) !!}
+        submitted
+        {!! Html::link($submission->extraction->url) !!}
+        <time class="timeago" datetime="{{ $submission->created_at->toIso8601String() }}"></time>
+    </p>
+    @if (isset($submissions[$i + 1])) <hr> @endif
 @endforeach
 @stop
