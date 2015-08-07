8e6c901ff44e123c5e8e38fa4027d40ce7aa0ff5
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-08-07 14:38:22 +0200

    updates link

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -2,5 +2,5 @@
     Made by {!! Html::link('http://www.wearenext.co.za', 'Next') !!} in Cape Town |
     {!! Html::link('terms-conditions', 'Terms & Conditions') !!} |
     {!! Html::link('privacy-policy', 'Privacy Policy') !!} |
-    {!! Html::link('https://github.com/we-are-next/larasites.com', 'Contribute on GitHub') !!}
+    {!! Html::link('https://github.com/we-are-next/www.larasites.com', 'Contribute on GitHub') !!}
 </p>
