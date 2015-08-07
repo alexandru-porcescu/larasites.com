19e4d240e7b424b3e74779789fc4e055358b0858
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-08-07 14:37:07 +0200

    jank

--- resources/views/add-site.blade.php
+++ resources/views/add-site.blade.php
@@ -1,6 +1,8 @@
 @extends('layout', ['title' => 'Add Site | Larasites.com'])
 
 @section('content')
+    <p>{!! Html::link($submission->extraction->url) !!} was submitted by {!! Html::link('https://www.twitter.com/@' . $submission->user->twitter_nickname, '@' . $submission->user->twitter_nickname) !!} <time class="timeago" datetime="{{ $submission->created_at->toIso8601String() }}"></time></p>
+    <hr>
     {!! Form::open(['method' => 'post', 'url' => action('SitesController@submitCreateForm')]) !!}
     <div class="form-group">
         <label>URL</label>
@@ -14,6 +16,7 @@
         <label>Description</label>
         {!! Form::textarea('description', $submission->extraction->body['description'], ['class' => 'form-control', 'rows' => 3]) !!}
     </div>
+    <hr>
     {!! Form::submit(null, ['class' => 'btn btn-default']) !!}
     {!! Form::close() !!}
 @stop
