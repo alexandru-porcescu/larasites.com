79004bd0dcca26d23fa101ff91ed6f7cdd73298a
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-08-11 18:12:18 +0200

    fixup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
         <div class="media">
             <div class="media-left" style="padding-left:10px;border-left:5px solid rgb({{ $site->rgb }})">
                 <a href="{{ $site->url }}" target="_blank" style="">
-                    <img class="media-object" src="{{ cloudinary_url(basename($site->image_url), ['width' => 150, 'height' => 150]) }}">
+                    <img class="media-object" src="{{ cloudinary_url(basename($site->image_url), ['width' => 150, 'height' => 150]) }}" width="150" height="150">
                 </a>
             </div>
             <div class="media-body">
