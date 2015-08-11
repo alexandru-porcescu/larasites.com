f2413fe122fa0bd84131bbca9de2d9b1d0457dd4
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-08-11 17:13:34 +0200

    fixes

--- resources/views/add-site.blade.php
+++ resources/views/add-site.blade.php
@@ -7,7 +7,7 @@
 
     <hr>
 
-    <a class="embedly-card" href="http://rawson.co.za">Rawson Property Group</a>
+    <a class="embedly-card" href="{{ $host->url }}">Embedly Preview</a>
     <script async src="//cdn.embedly.com/widgets/platform.js" charset="UTF-8"></script>
 
     <hr>
