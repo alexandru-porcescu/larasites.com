9018ff6d170e6aafdabd8067644ae8d9760a2be3
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-08-11 16:22:52 +0200

    adds user seeders back

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,7 +19,26 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        //
+        $users = [
+            User::create([
+                'twitter_id'       => '42204825',
+                'twitter_nickname' => 'waynethebrain',
+                'twitter_avatar'   => 'http://pbs.twimg.com/profile_images/555323316588670977/anbEvnry_normal.jpeg',
+                'is_admin'         => 1,
+            ]),
+            User::create([
+                'twitter_id'       => '4475091',
+                'twitter_nickname' => 'shawnroos',
+                'twitter_avatar'   => 'http://pbs.twimg.com/profile_images/514132007513632768/IW6GPity_normal.jpeg',
+                'is_admin'         => 1,
+            ]),
+            User::create([
+                'twitter_id'       => '79140061',
+                'twitter_nickname' => 'assertchris',
+                'twitter_avatar'   => 'https://pbs.twimg.com/profile_images/530731479421030400/1DWRjtfX.jpeg',
+                'is_admin'         => 1,
+            ])
+        ];
 
         Model::reguard();
     }
