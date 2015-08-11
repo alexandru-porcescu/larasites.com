83010e4695c22b79e24ef7cc970ebfacec625406
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-08-11 18:04:46 +0200

    adds user id to ga

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -28,6 +28,9 @@
             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
             })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+            @if (Auth::user())
+                ga('set', '&uid', '{{ Auth::user()->id }}');
+            @endif
             ga('create', 'UA-66193499-1', 'auto');
             ga('send', 'pageview');
         </script>
