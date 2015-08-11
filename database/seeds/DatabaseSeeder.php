dcc8be6410694db62406410524422e11eb2c339b
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-08-11 17:03:38 +0200

    seeder tweaks

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -25,18 +25,21 @@ class DatabaseSeeder extends Seeder
                 'twitter_nickname' => 'waynethebrain',
                 'twitter_avatar'   => 'http://pbs.twimg.com/profile_images/555323316588670977/anbEvnry_normal.jpeg',
                 'is_admin'         => 1,
+                'authenticated_at' => Carbon::now(),
             ]),
             User::create([
                 'twitter_id'       => '4475091',
                 'twitter_nickname' => 'shawnroos',
                 'twitter_avatar'   => 'http://pbs.twimg.com/profile_images/514132007513632768/IW6GPity_normal.jpeg',
                 'is_admin'         => 1,
+                'authenticated_at' => Carbon::now(),
             ]),
             User::create([
                 'twitter_id'       => '79140061',
                 'twitter_nickname' => 'assertchris',
                 'twitter_avatar'   => 'https://pbs.twimg.com/profile_images/530731479421030400/1DWRjtfX.jpeg',
                 'is_admin'         => 1,
+                'authenticated_at' => Carbon::now(),
             ])
         ];
 
