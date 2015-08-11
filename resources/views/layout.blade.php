60198d8026d8822dc95f06d2f187aaf002fc195b
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-08-11 18:05:07 +0200

    use twitter_id rather

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -29,7 +29,7 @@
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
             })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
             @if (Auth::user())
-                ga('set', '&uid', '{{ Auth::user()->id }}');
+                ga('set', '&uid', '{{ Auth::user()->twitter_id }}');
             @endif
             ga('create', 'UA-66193499-1', 'auto');
             ga('send', 'pageview');
