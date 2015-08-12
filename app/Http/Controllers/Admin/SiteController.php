f9d545d77a01718514456fdff58b9830dec73e7c
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-08-12 16:44:31 +0200

    creating sites

--- app/Http/Controllers/Admin/SiteController.php
+++ app/Http/Controllers/Admin/SiteController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Host;
+use App\Site;
+use Cloudinary\Uploader;
 
 class SiteController extends Controller
 {
@@ -22,9 +25,11 @@ class SiteController extends Controller
      *
      * @return Response
      */
-    public function create()
+    public function create(Request $request)
     {
-        //
+        $host = Host::where('name', $request->input('host'))->firstOrFail();
+
+        return view('admin.site.create', compact('host'));
     }
 
     /**
@@ -35,7 +40,38 @@ class SiteController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $host = Host::where('name', $request->input('host'))->firstOrFail();
+
+        $messages = [
+            'url_responds' => 'The :attribute responded with a non-200 status code, please make sure it\'s a valid url.'
+        ];
+
+        $this->validate($request, [
+            'url'         => ['required', 'url', 'active_url', 'unique:sites'],
+            'title'       => ['required', 'unique:sites'],
+            'description' => ['required'],
+            'image_url'   => ['required', 'url', 'url_responds'],
+            'red'         => ['required', 'numeric', 'min:0', 'max:255'],
+            'green'       => ['required', 'numeric', 'min:0', 'max:255'],
+            'blue'        => ['required', 'numeric', 'min:0', 'max:255'],
+        ], $messages);
+
+        $image = Uploader::upload($request->input('image_url'));
+        $site = new Site;
+        $site->url = $request->input('url');
+        $site->title = $request->input('title');
+        $site->description = $request->input('description');
+        $site->red = (int) $request->input('red');
+        $site->green = (int) $request->input('green');
+        $site->blue = (int) $request->input('blue');
+        $site->user_id = $host->submissions->first()->user->id;
+        $site->image_url = $image['secure_url'];
+        $site->save();
+
+        $host->site_id = $site->id;
+        $host->save();
+
+        return redirect()->action('Admin\SiteController@index');
     }
 
     /**
---
+++ resources/views/admin/site/create.blade.php
@@ -0,0 +1,48 @@
+@extends('admin.layout')
+
+@section('content')
+    <p class="lead">Create Site</p>
+    <hr>
+
+    @foreach ($errors->all() as $error)
+        <div class="alert alert-danger" role="alert">{{ $error }}</div>
+    @endforeach
+
+    {!! Form::open(['method' => 'post', 'url' => action('Admin\SiteController@store')]) !!}
+    {!! Form::hidden('host', $host->name) !!}
+
+    <div class="form-group {{ $errors->has('url') ? 'has-error' : '' }}">
+        <label class="control-label">URL</label>
+        {!! Form::text('url', $host->url, ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group {{ $errors->has('title') ? 'has-error' : '' }}">
+        <label class="control-label">Title</label>
+        {!! Form::text('title', null, ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group {{ $errors->has('description') ? 'has-error' : '' }}">
+        <label class="control-label">Description</label>
+        {!! Form::textarea('description', null, ['class' => 'form-control', 'rows' => 3]) !!}
+    </div>
+
+    <div class="form-group {{ $errors->has('image_url') ? 'has-error' : '' }}">
+        <label class="control-label">Image URL</label>
+        {!! Form::text('image_url', 'https://logo.clearbit.com/' . $host->name, ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group {{ $errors->has('red') || $errors->has('green') || $errors->has('blue') ? 'has-error' : '' }}">
+        <label class="control-label">RGB</label>
+        <div class="form-inline">
+            {!! Form::text('red', null, ['class' => 'form-control']) !!}
+            {!! Form::text('green', null, ['class' => 'form-control']) !!}
+            {!! Form::text('blue', null, ['class' => 'form-control']) !!}
+        </div>
+    </div>
+
+    <hr>
+
+    {!! Form::submit('Create', ['class' => 'btn btn-default']) !!}
+
+    {!! Form::close() !!}
+@stop
