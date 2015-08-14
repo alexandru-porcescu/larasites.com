2c30b819e13f11163373889ec31eea1133a3dd91
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-08-14 16:21:34 +0200

    footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,7 +1,29 @@
-<p class="text-muted">
-    Made by {!! Html::link('http://www.wearenext.co.za', 'Next') !!} in Cape Town |
-    {!! Html::link('terms-of-service', 'Terms of Service') !!} |
-    {!! Html::link('privacy-policy', 'Privacy Policy') !!} |
-    {!! Html::link('contributors-guide', 'Contributors Guide') !!} |
-    {!! Html::link('https://github.com/we-are-next/www.larasites.com', 'GitHub') !!}
-</p>
+<div class="row">
+    <div class="col-md-4">
+        <ul class="list-unstyled">
+            <li class="text-muted">Made by {!! Html::link('http://www.wearenext.co.za', 'Next') !!} in Cape Town</li>
+            <li>{!! Html::link('https://github.com/we-are-next/www.larasites.com', 'Form me on GitHub') !!}</li>
+        </ul>
+    </div>
+    <div class="col-md-4">
+        <ul class="list-unstyled">
+            <li><b class="text-muted">Legal</b></li>
+            <li>{!! Html::link('terms-of-service', 'Terms of Service') !!}</li>
+            <li>{!! Html::link('privacy-policy', 'Privacy Policy') !!}</li>
+            <li>{!! Html::link('contributors-guide', 'Contributors Guide') !!}</li>
+        </ul>
+    </div>
+    <div class="col-md-4">
+        <ul class="list-unstyled">
+            <li><b class="text-muted">Community</b></li>
+            <li>{!! Html::link('http://laravel.com', 'Laravel') !!}</li>
+            <li>{!! Html::link('http://lumen.laravel.com', 'Lumen') !!}</li>
+            <li>{!! Html::link('http://laravel.io/forum', 'Laravel.io') !!}</li>
+            <li>{!! Html::link('https://laracasts.com', 'Laracasts') !!}</li>
+            <li>{!! Html::link('https://laravel-news.com', 'Laravel News') !!}</li>
+            <li>{!! Html::link('https://laramap.com', 'Laramap') !!}</li>
+            <li>{!! Html::link('https://larajobs.com', 'Larajobs') !!}</li>
+            <li>{!! Html::link('https://larachat.co', 'Larachat') !!}</li>
+        </ul>
+    </div>
+</div>
