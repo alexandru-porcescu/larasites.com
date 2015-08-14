debaca72ebb6f72ce74317cf31d399c35a8a6592
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-08-14 15:23:31 +0200

    cleanup

--- resources/views/thankyou.blade.php
+++ resources/views/thankyou.blade.php
@@ -3,18 +3,4 @@
 @section('content')
     <p>Thanks for submitting your work to Larasites!</p>
     <p>{!! Html::link('submit', 'Submit another site', ['class' => 'btn btn-default']) !!}</p>
-    <hr>
-    <p>Why don't you visit one of these community sites while you wait for your site to be approved...</p>
-    <p>
-        <ul>
-            <li>{!! Html::link('http://laravel.com', 'laravel.com') !!}</li>
-            <li>{!! Html::link('http://lumen.laravel.com', 'lumen.laravel.com') !!}</li>
-            <li>{!! Html::link('http://laravel.io/forum', 'laravel.io') !!}</li>
-            <li>{!! Html::link('https://laramap.com', 'laramap.com') !!}</li>
-            <li>{!! Html::link('https://laracasts.com', 'laracasts.com') !!}</li>
-            <li>{!! Html::link('https://larajobs.com', 'larajobs.com') !!}</li>
-            <li>{!! Html::link('https://laravel-news.com', 'laravel-news.com') !!}</li>
-            <li>{!! Html::link('https://larachat.co', 'larachat.co') !!}</li>
-        </ul>
-    </p>
 @stop
