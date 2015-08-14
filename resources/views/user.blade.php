59d475141a83b8ffdd2c89f5066c6e1ce9beba89
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-08-14 15:21:03 +0200

    adds alt attributes

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,6 +1,6 @@
 @if ($user->cloudinary_public_id)
-    {!! Html::image(cloudinary_url($user->cloudinary_public_id, ['secure' => true, 'format' => 'jpg', 'width' => 20, 'height' => 20]), null, ['width' => 20, 'class' => 'img-circle']) !!}
+    {!! Html::image(cloudinary_url($user->cloudinary_public_id, ['secure' => true, 'format' => 'jpg', 'width' => 20, 'height' => 20]), '@' . $user->twitter_nickname, ['width' => 20, 'class' => 'img-circle']) !!}
 @elseif ($user->twitter_avatar)
-    {!! Html::image($user->twitter_avatar, null, ['width' => 20, 'class' => 'img-circle']) !!}
+    {!! Html::image($user->twitter_avatar, '@' . $user->twitter_nickname, ['width' => 20, 'class' => 'img-circle']) !!}
 @endif
 {!! Html::link('https://www.twitter.com/@' . $user->twitter_nickname, '@' . $user->twitter_nickname, ['target' => '_blank']) !!}
