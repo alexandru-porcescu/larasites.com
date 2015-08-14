b124b795fb6bee92e3700cfadde7a461245e05c8
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-08-14 15:29:57 +0200

    #design

--- resources/views/thankyou.blade.php
+++ resources/views/thankyou.blade.php
@@ -1,6 +1,9 @@
 @extends('layout', ['title' => 'Submit a site | Larasites.com'])
 
 @section('content')
-    <p>Thanks for submitting your work to Larasites!</p>
-    <p>{!! Html::link('submit', 'Submit another site', ['class' => 'btn btn-default']) !!}</p>
+    <center>
+        <h4>Thanks for submitting your work to Larasites!</h4>
+        <br>
+        <p>{!! Html::link('submit', 'Submit another site', ['class' => 'btn btn-default']) !!}</p>
+    </center>
 @stop
