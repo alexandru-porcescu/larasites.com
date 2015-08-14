2966e6bedd25784a4bc1351d0dfd7297623c2012
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-08-14 15:36:37 +0200

    adds test for seeing approved sites

--- tests/AcceptanceTest.php
+++ tests/AcceptanceTest.php
@@ -3,20 +3,40 @@
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Carbon\Carbon;
 use App\User;
 use App\Host;
+use App\Site;
 use App\Submission;
 
-class SubmissionTest extends TestCase
+class AcceptanceTest extends TestCase
 {
     use DatabaseMigrations;
 
-    /**
-     * A basic functional test example.
-     *
-     * @return void
-     */
-    public function testBasicExample()
+    public function testApprovedSites()
+    {
+        $user = new User;
+        $user->twitter_id = 1;
+        $user->twitter_nickname = 'laravelphp';
+        $user->twitter_avatar = '...';
+        $user->twitter_avatar_original = '...';
+        $user->save();
+
+        $site = new Site;
+        $site->url = 'http://larabelle.com';
+        $site->title = 'Larabelle';
+        $site->description = '...';
+        $site->image_url = '...';
+        $site->approved_at = Carbon::now();
+        $site->approved_by = $user->id;
+        $site->user_id = $user->id;
+        $site->save();
+
+        $this->visit('/')->see('Larabelle');
+        $this->visit('/')->see('http://larabelle.com');
+    }
+
+    public function testSubmittingSites()
     {
         $user = new User;
         $user->twitter_id = 1337;
