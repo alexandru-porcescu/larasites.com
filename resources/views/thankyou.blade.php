4673c3d892132a1db6bb48175a07d4c263e3458b
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-08-21 11:17:04 +0200

    thank you page

--- resources/views/thankyou.blade.php
+++ resources/views/thankyou.blade.php
@@ -1,9 +1,15 @@
 @extends('layout', ['title' => 'Submit a site | Larasites.com'])
 
 @section('content')
-    <center>
-        <h4>Thanks for submitting your work to Larasites!</h4>
-        <br>
-        <p>{!! Html::link('submit', 'Submit another site', ['class' => 'btn btn-default']) !!}</p>
-    </center>
+<div class="l-section l-section--flat">
+    <div class="l-wrapper l-wrapper--center">
+        <img class="l-image" src="{{ url('assets/images/high-five-855x211.gif') }}" width="855" height="211" alt="Eureka!">
+
+        <p>We’re reviewing your submission and will tweet you when your Larasite is up.</p>
+
+        <a class="btn btn--icon" href="https://twitter.com/intent/tweet?text=Larasites – Showcasing the best websites made using Laravel and Lumen https%3A%2F%2Fwww.larasites.com&via=larasites" target="_blank">
+            <span class="icon icon--left fa fa-twitter" aria-hidden="true"></span> Spread the word
+        </a>
+    </div>
+</div>
 @stop
