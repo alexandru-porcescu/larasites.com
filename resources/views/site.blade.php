1f15225d0297e11aec7badf2bc89f4226d4d6c69
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-08-21 12:12:38 +0200

    crop mode

---
+++ resources/views/site.blade.php
@@ -0,0 +1,29 @@
+<div class="card">
+    <div class="card__logo" style="background-color: {{ '#'.$site->color->getHex() }}">
+        <a href="{{ $site->url }}">
+            <img src="{{ cloudinary_url($site->cloudinary_public_id, ['crop' => 'fit', 'secure' => true, 'width' => 150, 'height' => 150, 'format' => 'png']) }}" width="150" height="150" alt="">
+        </a>
+        <div class="card-vote">
+            <a class="card-vote__link" href="{{ action('VotingController@submitVote', [$site->id]) }}">
+                <span class="icon fa fa-heart" aria-hidden="true"></span>
+                <span class="card-vote__count">{{ $site->vote_count }}</span>
+            </a>
+        </div>
+    </div>
+    <div class="card-contents">
+        <h2 class="card__title">
+            <a href="{{ $site->url }}">{{ $site->title }}</a>
+        </h2>
+        <p class="card__synopsis">{{ $site->description }}</p>
+        <div class="meta">
+            @if ($site->user->cloudinary_public_id)
+                {!! Html::image(cloudinary_url($site->user->cloudinary_public_id, ['secure' => true, 'format' => 'jpg', 'width' => 30, 'height' => 30]), '@' . $site->user->twitter_nickname, ['width' => 30, 'height' => 30, 'class' => 'meta__img']) !!}
+            @elseif ($site->user->twitter_avatar)
+                {!! Html::image($site->user->twitter_avatar, '@' . $site->user->twitter_nickname, ['width' => 30, 'height' => 30, 'class' => 'meta__img']) !!}
+            @endif
+            <p class="meta__synopsis">
+                <a href="{{ $site->url }}">{{ $site->host }}</a> submitted by <a href="https://twitter.com/{{ $site->user->twitter_nickname }}">{{ '@' . $site->user->twitter_nickname }}</a>
+            </p>
+        </div>
+    </div>
+</div>
