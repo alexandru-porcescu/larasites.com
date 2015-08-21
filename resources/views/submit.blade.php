150d89e9425e2144bff817e6ff62c7835a114e5d
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-08-21 11:05:27 +0200

    submit page

--- resources/views/submit.blade.php
+++ resources/views/submit.blade.php
@@ -1,13 +1,20 @@
 @extends('layout', ['title' => 'Submit a site | Larasites.com'])
 
 @section('content')
-    {!! Form::open(['method' => 'post', 'url' => '/submit/submit']) !!}
-    <div class="form-group {{ count($errors) > 0 ? 'has-error' : '' }}">
-        {!! Form::text('url', null, ['class' => 'form-control', 'autofocus' => 'autofocus', 'placeholder' => 'eg. http://laravel.com']) !!}
-        @if (count($errors) > 0)
-            <p class="help-block">{{ $errors->first() }}</p>
-        @endif
-    </div>
-    {!! Form::submit('Submit', ['class' => 'btn btn-default']) !!}
-    {!! Form::close() !!}
+    <main class="main" role="main">
+        <div class="l-section l-section--background" style="background-image: url('{{ url('assets/images/background-submit-a-site-1722x664.png') }}');">
+            <div class="l-wrapper">
+                {!! Form::open(['name' => 'form-submit', 'id' => 'form-submit', 'method' => 'post', 'url' => '/submit/submit', 'class' => 'form form--well']) !!}
+                    <label class="u-visuallyhidden" for="url">URL</label>
+                    <input class="input" type="text" id="url" name="url" placeholder="http://" maxlength="100" value="{{ old('url') }}" autofocus>
+
+                    <input class="btn" type="submit" id="submit" name="submit" value="Submit">
+
+                    @if (count($errors) > 0)
+                        <p>{{ $errors->first() }}</p>
+                    @endif
+                {!! Form::close() !!}
+            </div>
+        </div>
+    </main>
 @stop
