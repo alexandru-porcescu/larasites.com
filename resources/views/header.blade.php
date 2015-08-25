e5e2e0238a60f36b054227940704a96b14f95864
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-08-25 11:10:14 +0200

    adds strapline

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -3,9 +3,9 @@
         <a class="logo" href="{{ url('/') }}">
             <img src="{{ url('assets/images/logo-larasites-162x53.png') }}" srcset="{{ url('assets/images/logo-larasites-324x106.png') }} 2x" width="162" height="53" alt="Larasites logo">
         </a>
-
         <a class="btn btn--white btn--icon" href="{{ action('SubmitController@showSubmitForm') }}">
             <span class="icon icon--left fa fa-twitter" aria-hidden="true"></span> Submit <span class="u-hide--small">a Site</span>
         </a>
+        <h1 class="strapline">The world’s best websites made with <strong>Laravel</strong>.</h1>
     </div>
 </header>
