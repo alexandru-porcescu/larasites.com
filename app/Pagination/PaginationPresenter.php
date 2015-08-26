09d3d4f93bb465ac90ace7ca244ec5c662bc2643
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-08-26 15:13:21 +0200

    pagination wip

--- app/Pagination/PaginationPresenter.php
+++ app/Pagination/PaginationPresenter.php
@@ -46,4 +46,46 @@ class PaginationPresenter extends BootstrapThreePresenter
     {
         return '<li class="is-disabled"><a href="#">'.$text.'</a></li>';
     }
+
+    /**
+     * Get the previous page pagination element.
+     *
+     * @param  string  $text
+     * @return string
+     */
+    public function getPreviousButton($text = '&laquo;')
+    {
+        // If the current page is less than or equal to one, it means we can't go any
+        // further back in the pages, so we will render a disabled previous button
+        // when that is the case. Otherwise, we will give it an active "status".
+        if ($this->paginator->currentPage() <= 1) {
+            return '<li class="is-disabled"><span class="icon fa fa-chevron-left" aria-hidden="true"></span>Prev</li>';
+        }
+
+        $url = $this->paginator->url(
+            $this->paginator->currentPage() - 1
+        );
+
+        return '<li><a href="'.$url.'"><span class="icon fa fa-chevron-left" aria-hidden="true"></span>Prev</a></li>';
+    }
+
+    /**
+     * Get the next page pagination element.
+     *
+     * @param  string  $text
+     * @return string
+     */
+    public function getNextButton($text = '&raquo;')
+    {
+        // If the current page is greater than or equal to the last page, it means we
+        // can't go any further into the pages, as we're already on this last page
+        // that is available, so we will make it the "next" link style disabled.
+        if (! $this->paginator->hasMorePages()) {
+            return '<li class="is-disabled">Next<span class="icon fa fa-chevron-right" aria-hidden="true"></span></li>';
+        }
+
+        $url = $this->paginator->url($this->paginator->currentPage() + 1);
+
+        return '<li><a href="'.$url.'">Next<span class="icon fa fa-chevron-right" aria-hidden="true"></span></a></li>';
+    }
 }
