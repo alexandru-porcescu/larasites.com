39482593369867e548c2e29e189bdfcc68a7c6bb
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-08-26 13:20:07 +0200

    removes google analytics from admin pages

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -19,14 +19,5 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-timeago/1.4.1/jquery.timeago.min.js"></script>
         <script>$('time.timeago').timeago();</script>
-        <script>
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-            ga('set', '&uid', '{{ Auth::user()->twitter_id }}');
-            ga('create', 'UA-66193499-1', 'auto');
-            ga('send', 'pageview');
-        </script>
     </body>
 </html>
