f0de3ec725987d11e981bed4f136aad370b1a204
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-08-27 14:32:26 +0200

    renames view

---
+++ resources/views/sites.blade.php
@@ -0,0 +1,24 @@
+@extends('layout')
+
+@section('head')
+    @foreach ($sites as $site)
+        @include('site-css', compact('site'))
+    @endforeach
+@stop
+
+@section('content')
+@include('nav')
+
+<main class="main" role="main">
+    <div class="l-section">
+        <div class="l-wrapper">
+            <div class="cards">
+                @foreach ($sites as $site)
+                    @include('site', compact('site'))
+                @endforeach
+            </div><!-- .cards -->
+            {!! $paginator->render() !!}
+        </div><!-- .l-wrapper -->
+    </div><!-- .l-section -->
+</main>
+@stop
