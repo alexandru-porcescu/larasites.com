263ea0ce510bae20a079f1e62eede912180b29eb
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-08-27 12:40:55 +0200

    cleanup

--- resources/views/admin/site/index.blade.php
+++ resources/views/admin/site/index.blade.php
@@ -13,7 +13,25 @@
 
     @foreach ($sites as $key => $site)
         @if ($key > 0) <hr> @endif
-        @include('admin.site.site', compact('site'))
+
+        <div class="media">
+            <div class="media-left">
+                <a href="{{ action('Admin\SiteController@show', [$site->id]) }}" target="_blank" style="">
+                    <img class="media-object" src="{{ cloudinary_url($site->cloudinary_public_id, ['secure' => true, 'width' => 100]) }}">
+                </a>
+            </div>
+            <div class="media-body">
+                <h4 class="media-heading">{!! Html::linkAction('Admin\SiteController@show', $site->title, [$site->id]) !!}</h4>
+                <p>{{ str_limit($site->description, 90) }}</p>
+                <small>
+                    @if ($site->approved_at)
+                        <a href="#" class="btn btn-sm btn-secondary disabled">Approved {!! timeago($site->approved_at) !!}</a>
+                    @else
+                        {!! Html::linkAction('Admin\SiteController@approve', 'Approve', [$site->id], ['class' => 'btn btn-sm btn-secondary']) !!}
+                    @endif
+                </small>
+            </div>
+        </div>
     @endforeach
 
     <br>
