2cfbc17beb6c166fa15860db4751c81054c66491
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-08-28 10:49:02 +0200

    cleanup

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,12 +2,8 @@
 
 /*
 |--------------------------------------------------------------------------
-| Application Routes
+| Laravel.com routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
 |
 */
 
