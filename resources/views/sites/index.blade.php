50791e5680759daf2c74dbd7eec4a6369b59bda6
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-08-28 10:42:13 +0200

    cleanup

--- resources/views/sites/index.blade.php
+++ resources/views/sites/index.blade.php
@@ -7,18 +7,18 @@
 @stop
 
 @section('content')
-@include('nav')
+    @include('nav')
 
-<main class="main" role="main">
-    <div class="l-section">
-        <div class="l-wrapper">
-            <div class="cards">
-                @foreach ($sites as $site)
-                    @include('sites.site', compact('site'))
-                @endforeach
-            </div><!-- .cards -->
-            {!! $paginator->render() !!}
-        </div><!-- .l-wrapper -->
-    </div><!-- .l-section -->
-</main>
+    <main class="main" role="main">
+        <div class="l-section">
+            <div class="l-wrapper">
+                <div class="cards">
+                    @foreach ($sites as $site)
+                        @include('sites.site', compact('site'))
+                    @endforeach
+                </div><!-- .cards -->
+                {!! $paginator->render() !!}
+            </div><!-- .l-wrapper -->
+        </div><!-- .l-section -->
+    </main>
 @stop
