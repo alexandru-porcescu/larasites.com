ab90cc7586e1adb5ed8b08f3bfa0e613b102c065
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-08-31 16:04:25 +0200

    removes superfluous import

--- app/Http/Controllers/SitesController.php
+++ app/Http/Controllers/SitesController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Auth;
 use App\Site;
-use App\Host;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Pagination\PaginationPresenter;
