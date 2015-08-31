fe6ddd626ee078c24a070de47892adf72669bb9d
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-08-31 16:33:28 +0200

    fixes sitemap

--- resources/views/sitemap/index.blade.php
+++ resources/views/sitemap/index.blade.php
@@ -1,8 +1,8 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL; ?>
-<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  @foreach ($routes as $route)
-  <sitemap>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+@foreach ($routes as $route)
+  <url>
     <loc>{{ $route }}</loc>
-  </sitemap>
-  @endforeach
-</sitemapindex>
+  </url>
+@endforeach
+</urlset>
