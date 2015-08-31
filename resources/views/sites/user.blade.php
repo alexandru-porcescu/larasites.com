165e5e68061cfe78d3836c3196d5229f8f301531
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-08-31 15:32:02 +0200

    switch user order

--- resources/views/sites/user.blade.php
+++ resources/views/sites/user.blade.php
@@ -11,9 +11,9 @@
 <p class="meta__synopsis">
     <a href="{{ $site->url }}" target="_blank">
         {{ League\Url\Url::createFromUrl($site->url)->getHost() }}</a>
-        submitted by
-        <a href="https://twitter.com/{{ $site->user->twitter_nickname }}">{{ '@' . $site->user->twitter_nickname }}</a>@if ($site->builder),
-        made by <a href="https://twitter.com/{{ $site->builder->twitter_nickname }}">{{ '@' . $site->builder->twitter_nickname }}</a>
+        @if ($site->builder)
+            made by <a href="https://twitter.com/{{ $site->builder->twitter_nickname }}">{{ '@' . $site->builder->twitter_nickname }}</a>,
         @endif
+        submitted by <a href="https://twitter.com/{{ $site->user->twitter_nickname }}">{{ '@' . $site->user->twitter_nickname }}</a>
     </a>
 </p>
