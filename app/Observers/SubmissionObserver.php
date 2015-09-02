3ed0e2103e22ad9c2f62d8503dd52cf578205d31
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2015-09-02 11:44:46 +0200

    cleanup

--- app/Observers/SubmissionObserver.php
+++ app/Observers/SubmissionObserver.php
@@ -7,14 +7,13 @@ use League\Url\UrlImmutable;
 
 class SubmissionObserver
 {
-
     public function saving($model)
     {
         $url = UrlImmutable::createFromUrl($model->url);
         $hostUrl = $url->getHost();
         $host = Host::withTrashed()
-        ->where('name', (string) $hostUrl)
-        ->first();
+            ->where('name', (string) $hostUrl)
+            ->first();
 
         if (! $host) {
             $host = new Host;
@@ -23,9 +22,5 @@ class SubmissionObserver
         }
 
         $model->host_id = $host->id;
-    }
-
-    public function saved($model)
-    {
     }
 }
